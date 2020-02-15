1e3fab067eddaa8b6977c7c31a7532a9f353f82f
ink8bit <ink8bit@users.noreply.github.com>
2020-02-15 13:11:59 +0300

    Change ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,59 @@
+/**
+ * Configuring ESLint
+ * @see {@link https://eslint.org/docs/user-guide/configuring}
+ */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-environments}
+   */
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#extending-configuration-files}
+   */
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'eslint-config-prettier',
+  ],
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-globals}
+   */
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-parser}
+   */
+  parser: '@typescript-eslint/parser',
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#specifying-parser-options}
+   */
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#configuring-plugins}
+   */
+  plugins: ['@typescript-eslint'],
+
+  /**
+   * @see {@link https://eslint.org/docs/user-guide/configuring#configuring-rules}
+   */
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
