613bb6cba96ccbdb4cc4f32ca06795d026d66e76
ink8bit <ink8bit@users.noreply.github.com>
2020-02-15 15:18:24 +0300

    Change babel config

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,9 @@ const isTest = process.env.NODE_ENV === 'test';
 module.exports = {
   presets: [
     [
+      /**
+       * @see {@link https://babeljs.io/docs/en/babel-preset-env}
+       */
       '@babel/preset-env',
       {
         modules: isTest ? 'commonjs' : false,
@@ -14,8 +17,15 @@ module.exports = {
         },
       },
     ],
-    '@babel/preset-react',
+
+    /**
+     * @see {@link https://babeljs.io/docs/en/babel-preset-typescript}
+     */
     '@babel/preset-typescript',
   ],
+
+  /**
+   * @see {@link https://babeljs.io/docs/en/babel-plugin-transform-runtime}
+   */
   plugins: ['@babel/plugin-transform-runtime'],
 };
