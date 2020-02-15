381ef4173cbc74a8dce499935e5de220e51be438
ink8bit <ink8bit@users.noreply.github.com>
2020-02-15 14:19:49 +0300

    Add jest

---
+++ jest.config.ci.js
@@ -0,0 +1,10 @@
+/**
+ * @see {@link https://jestjs.io/docs/en/configuration}
+ */
+module.exports = {
+  notify: false,
+  bail: false,
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
+};
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/**
+ * @see {@link https://jestjs.io/docs/en/configuration}
+ */
+module.exports = {
+  notify: true,
+  bail: true,
+  verbose: false,
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
+};
