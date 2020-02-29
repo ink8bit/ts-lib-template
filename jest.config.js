e6f7ec8adb7b5a505856e3ba6482e54ce1e519c5
ink8bit <ink8bit@users.noreply.github.com>
2020-02-29 12:13:12 +0300

    Change jest configs
    Added common config to avoid duplication.
    Added coverage threshold as an example.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
+const jestCommonConfig = require('./jest.config.common');
+
 /**
  * @see {@link https://jestjs.io/docs/en/configuration}
  */
 module.exports = {
+  ...jestCommonConfig,
   displayName: 'local',
   notify: true,
   bail: true,
