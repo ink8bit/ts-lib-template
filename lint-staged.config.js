16c47d5a11821a2cf9a50e915bed98c9949032d0
ink8bit <ink8bit@users.noreply.github.com>
2020-03-15 14:55:09 +0300

    Add fix option to ESLint on pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@
  * @see {@link https://github.com/okonet/lint-staged#configuration}
  */
 module.exports = {
-  '**/*.ts': ['tsc', 'prettier --write', 'eslint'],
-  '**/*.js': ['prettier --write', 'eslint'],
+  '**/*.ts': ['tsc', 'prettier --write', 'eslint --fix'],
+  '**/*.js': ['prettier --write', 'eslint --fix'],
   '**/*.json': ['prettier --write'],
 };
